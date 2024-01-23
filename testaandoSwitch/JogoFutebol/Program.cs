7c5488f7fd334da5974dc5795d64c1214d811fff
Maikon Sposito <111329558+Maiko552@users.noreply.github.com>
2024-01-23 16:35:58 -0300

    Add files via upload

---
+++ testaandoSwitch/JogoFutebol/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoFutebol
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, resul = 0;
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("  Santos x Conrinthians  ");
+            Console.WriteLine("-------------------------");
+
+            Console.WriteLine("Quantos gols o Santos fez?");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Quantos gols o Corithians fez?");
+            y = int.Parse(Console.ReadLine());
+
+            resul = x - y;
+
+            switch (resul)
+            {
+                case 0: Console.WriteLine("\nEmpate");break;
+                case 1: Console.WriteLine("\nPartida Normal");break;
+                case 2: Console.WriteLine("\nPartida Normal");break;
+                case 3: Console.WriteLine("\nPatida Normal");break;
+                case 4: Console.WriteLine("\nGoleada");break;
+                case 5: Console.WriteLine("\nGoleada");break;
+                default: Console.WriteLine("\nDeream um show");break;
+                  
+             }
+            if (resul < 0) 
+            {
+                Console.WriteLine("Numero Invalido");
+            }
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Diferença de {0} gols", resul);
+            Console.WriteLine("-------------------------------");
+
+            Console.ReadKey();
+
+        }
+
+    
+            
+
+
+        
+    }
+}
---
+++ testaandoSwitch/testaandoSwitch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testaandoSwitch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int valor = 0;
+
+            Console.WriteLine("------ CRIANÇA ESPERANÇA ------");
+            Console.WriteLine("Muito obrigado por ajudar!");
+
+            Console.WriteLine("{1} Para doar R$5 ");
+            Console.WriteLine("{2} Para doar R$10 ");
+            Console.WriteLine("{3} Para doar R$15 ");
+            Console.WriteLine("{4} Para doar R$20 ");
+            Console.WriteLine("{5} Para doar outros valores ");
+            Console.WriteLine("{6} Para cancelar");
+            Console.WriteLine("-------------------------------");
+            N = int.Parse(Console.ReadLine());
+
+            switch (N)
+            {
+                case 1: valor = 5;break;
+                case 2: valor = 10;break;
+                case 3: valor = 15;break;
+                case 4: valor = 20;break;
+                case 5: Console.WriteLine("Qual o valor da doação?");
+                    valor = int.Parse(Console.ReadLine());break;
+                case 6: valor = 0;break;
+                default: Console.WriteLine("Numero Inválido");break;
+            }
+
+            Console.WriteLine("VOCÊ DOOU R${0}, MUITO OBRIGADO", valor);
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
